7b198471e9d0a86fbd0d50316580cd74bf61005f
Mitchell-610 <Mheit602@gmail.com>
2024-10-20 14:26:12 -0400

    Resolvers updated with correct code.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,69 +1,79 @@
-const { User, Book} = require('../models');
-const mongoose = require('mongoose');
+// resolvers/index.js
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    Query: {
-        User: async () => {
-            const users = await User.find({}).populate('books');
-            return users.map(user => ({
-              ...user.toObject(),
-              savedBooks: user.savedBooks || []
-            }));
-          },
-          oneUser: async (parent, { UserId }) => {
-            return User.findOne({ _id: UserId });
-          },
-          Book: async () => {
-            const data = Book.find({});
-            console.log(data);
-            return Book.find({});
-          },
-          OneBook: async (parent, { bookId }) => {
-            console.log(book);
-            return Book.findOne({ _id: bookId });
-          },
-        },
-        Mutation: {
-            addUser: async (parent, { email, userName, password }) => {
-              try {
-                const existingUser = await User.findOne({ userName });
-                if (existingUser) {
-                  throw new Error('Username already exists');
-                }
-                const newUser = new User({ email, userName, password });
-                console.log(newUser);
-                const savedUser = await newUser.save();
-                return savedUser;
-              } catch (error) {
-                console.error('Error adding user:', error);
-                throw new Error(`Failed to add user: ${error.message}`);
-              }
-            },
-            addBook: async (parent, { UserId, book }) => {
-              try {
-                const updatedUser = await User.findOneAndUpdate(
-                  { _id: UserId },
-                  { $addToSet: { books: book } },
-                  { new: true, runValidators: true }
-                );
-                
-                if (!updatedUser) {
-                  throw new Error('User not found');
-                }
-                
-                return updatedUser;
-              } catch (error) {
-                throw new Error(`Failed to add skill: ${error.message}`);
-              }
-            },
-            removeBook: async (parent, { UserId, book }) => {
-              return User.findOneAndUpdate(
-                { _id: UserId },
-                { $pull: { books: book } },
-                { new: true }
-              );
-            },
-          },
-    };
+  Query: {
+    // Get a single user by ID
+    async getSingleUser(_, { id }, context) {
+      const foundUser = await User.findOne({ _id: id });
 
-    module.exports = resolvers;
+      if (!foundUser) {
+        throw new Error('Cannot find a user with this id!');
+      }
+
+      return foundUser;
+    },
+  },
+  Mutation: {
+    // Create a user and sign a token
+    async createUser(_, { username, email, password }) {
+      const user = await User.create({ username, email, password });
+
+      if (!user) {
+        throw new Error('Something is wrong!');
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    
+    // Login a user and sign a token
+    async login(_, { username, email, password }) {
+      const user = await User.findOne({ $or: [{ username }, { email }] });
+      if (!user) {
+        throw new Error("Can't find this user");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new Error('Wrong password!');
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    
+    // Save a book to a user's savedBooks
+    async saveBook(_, { bookId }, context) {
+      if (!context.user) {
+        throw new Error('You need to be logged in!');
+      }
+
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $addToSet: { savedBooks: { bookId } } },
+        { new: true, runValidators: true }
+      );
+      return updatedUser;
+    },
+
+    // Remove a book from savedBooks
+    async deleteBook(_, { bookId }, context) {
+      if (!context.user) {
+        throw new Error('You need to be logged in!');
+      }
+
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: { bookId } } },
+        { new: true }
+      );
+      if (!updatedUser) {
+        throw new Error("Couldn't find user with this id!");
+      }
+      return updatedUser;
+    },
+  },
+};
+
+module.exports = resolvers;
